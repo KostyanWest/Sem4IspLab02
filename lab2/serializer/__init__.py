74385c3b0446350fae3e51e1e9f07feb01a0b189
KostyanWest <kostyanwest@gmail.com>
2021-06-03 22:16:57 +0300

    Add pickle.py

--- lab2/serializer/__init__.py
+++ lab2/serializer/__init__.py
@@ -1,10 +1,11 @@
 from .serialize_tools import *
 from . import json
+from . import pickle
 
 
 _langs = {}
 
-for module in [json]:
+for module in [json, pickle]:
     for ext in set(map(str.lower, module.get_extentions())):
        if not _langs.setdefault(ext, module) is module:
            raise ValueError("Both '{}' and '{}' uses '{}' extention".format(_langs[ext].__name__,
