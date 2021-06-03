ad8999d831f212758ed78ffa67365bb9eddcee2e
KostyanWest <kostyanwest@gmail.com>
2021-06-03 22:15:42 +0300

    Add __init__.py to serializer

---
+++ lab2/serializer/__init__.py
@@ -0,0 +1,23 @@
+from .serialize_tools import *
+from . import json
+
+
+_langs = {}
+
+for module in [json]:
+    for ext in set(map(str.lower, module.get_extentions())):
+       if not _langs.setdefault(ext, module) is module:
+           raise ValueError("Both '{}' and '{}' uses '{}' extention".format(_langs[ext].__name__,
+                                                                            module.__name__,
+                                                                            ext))
+
+
+def create_serializer(lang):
+    try:
+        return _langs[lang]
+    except KeyError:
+        raise ValueError("The lang '{}' is not supported".format(lang))
+
+
+def get_langs():
+    return list(_langs)
